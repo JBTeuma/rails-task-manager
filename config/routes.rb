b18416ff4d7cf91ea7324ea8ec65b5fd31e85891
JeanBatiste Teuma <teuma.jeanbaptiste@gmail.com>
2021-11-09 13:13:30 +0100

    created task manager cru not d

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
+# frozen_string_literal: true
+
+# Class
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/tasks/new', to: 'tasks#new'
+  post '/tasks', to: 'tasks#create'
+
+  get '/tasks', to: 'tasks#index'
+
+  get '/task/:id', to: 'tasks#show', as: 'task'
+
+  get '/task/:id/edit', to: 'tasks#edit'
+  patch '/task/:id', to: 'tasks#update'
 end
