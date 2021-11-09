ad363e6d6c7a26bd082554353e0706e36d1b21f2
JeanBatiste Teuma <teuma.jeanbaptiste@gmail.com>
2021-11-09 17:02:20 +0100

    refactored

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,5 @@
 # Class
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/tasks/new', to: 'tasks#new'
-  post '/tasks', to: 'tasks#create'
-
-  get '/tasks', to: 'tasks#index'
-
-  get '/task/:id', to: 'tasks#show', as: 'task'
-
-  get '/task/:id/edit', to: 'tasks#edit'
-  patch '/task/:id', to: 'tasks#update'
-
-  delete '/task/:id', to: 'tasks#destroy'
+  resources :tasks
 end
